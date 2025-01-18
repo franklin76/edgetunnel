8540ed195d5cee51d34b5b30fa8f50b1b2c1d8f9
franklin76 <44045559+franklin76@users.noreply.github.com>
2025-01-18 13:39:33 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'ee3e67bf-b0d3-4fbf-8f54-1d5f75590773';
 let proxyIP = '';
 //let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
